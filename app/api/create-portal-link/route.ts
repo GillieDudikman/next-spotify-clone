4ac132b0fb70284d74df530461a527a6aa2c5352
gillie dudikman <gilliedudik@gmail.com>
2023-10-05 01:42:43 +0300

    added supabase and stripe connection

---
+++ app/api/create-portal-link/route.ts
@@ -0,0 +1,31 @@
+import {createRouteHandlerClient} from "@supabase/auth-helpers-nextjs";
+import {cookies} from "next/headers";
+import {createOrRetrieveCustomer} from "@/libs/supabaseAdmin";
+import {stripe} from "@/libs/stripe";
+import {getUrl} from "@/libs/helpers";
+import {NextResponse} from "next/server";
+
+export async function POST(){
+    try{
+        const supabase = createRouteHandlerClient({
+            cookies
+        })
+        const { data: { user }} = await supabase.auth.getUser();
+        if(!user)
+            throw new Error("Could not get user");
+
+        const customer = await createOrRetrieveCustomer({
+            uuid: user.id || '',
+            email: user.email || ''
+        })
+        if(!customer) throw new Error("Could not get customer");
+
+        const { url } = await stripe.billingPortal.sessions.create({
+            customer,
+            return_url: `${getUrl()}/account`
+        })
+        return NextResponse.json({ url })
+    }catch(error: any){
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
