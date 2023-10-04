f027f968fd2c176a09182cf32dee56e07a3eb5ba
gillie dudikman <gilliedudik@gmail.com>
2023-10-05 01:16:51 +0300

    added payment method using Stripe

---
+++ libs/helpers.ts
@@ -0,0 +1,34 @@
+import {Price} from "@/types";
+
+export const getUrl = () => {
+    let url =
+        process.env.NEXT_PUBLIC_SITE_URL ??
+        process.env.NEXT_PUBLIC_VERCEL_URL ??
+        'http://localhost:3000';
+
+    url = url.includes('http') ? url : `https://${url}`;
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+
+    return url
+}
+
+export const postData = async ({ url, data }: { url: string; data?: { price:Price }}) => {
+    const res:Response = await fetch(url, {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json'}),
+        credentials: 'same-origin',
+        body: JSON.stringify(data)
+    })
+
+    if(!res.ok) {
+        console.log("error in post", {url, data, res})
+        throw new Error(res.statusText)
+    }
+    return res.json();
+}
+
+export const toDateTime = (seconds: number) => {
+    var t = new Date('1970-1-1T00:30:00Z');
+    t.setSeconds(seconds);
+    return t;
+}
